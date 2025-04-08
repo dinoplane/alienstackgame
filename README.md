9fca85d46f95a38392d6a050a7c088682c748f79
dinoplane <isacsac2017@gmail.com>
2025-04-08 03:00:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+db                  - player positions / player ids / ip
+backend             - processes player requests
+frontend            - game itself
+
---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+import Canvas from "./Canvas";
+
+// import { create, globals } from 'webgpu';
+
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+
+  }, []);
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Canvas/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
